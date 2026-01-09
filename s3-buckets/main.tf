e8967d7e825f9c8d6bad3477fec506288b2e86a5
Mecheri Razane <168592564+mecherirazane@users.noreply.github.com>
2026-01-09 18:39:05 +0100

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -2,11 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_id" "suffix1" {
+  byte_length = 4
+}
+
+resource "random_id" "suffix2" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "razane-eks-bucket1-${random_id.suffix1.hex}"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "razane-eks-bucket1-${random_id.suffix1.hex}"
     Environment = "dev"
   }
 }
@@ -19,10 +27,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "razane-eks-bucket2-${random_id.suffix2.hex}"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "razane-eks-bucket2-${random_id.suffix2.hex}"
     Environment = "dev"
   }
 }
