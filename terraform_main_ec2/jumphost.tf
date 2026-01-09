5e698db42e75f9646ffac5e3c790a3dd22395e6d
Mecheri Razane <168592564+mecherirazane@users.noreply.github.com>
2026-01-09 20:32:53 +0100

    Update jumphost.tf

--- terraform_main_ec2/jumphost.tf
+++ terraform_main_ec2/jumphost.tf
@@ -1,13 +1,22 @@
 resource "aws_instance" "ec2" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  # استخدمي اسم المفتاح الذي أنشأتِه في AWS Console (مثلاً riheb-key)
+  key_name = "razane-key"
+
   subnet_id              = aws_subnet.public-subnet1.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
-  iam_instance_profile = aws_iam_instance_profile.instance-profile.name
+
+  # في Learner Lab لا يمكنك إنشاء IAM Instance Profile جديد
+  # لذلك إمّا تحذفي هذا السطر أو تضعي اسم Instance Profile موجود مسبقًا
+  # إذا كان عندك LabRole مرتبط بـ Instance Profile، اكتبيه هنا
+  # iam_instance_profile = "LabInstanceProfile"
+
   root_block_device {
     volume_size = 30
   }
+
   user_data = templatefile("./install-tools.sh", {})
 
   tags = {
