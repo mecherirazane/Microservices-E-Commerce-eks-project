9607608b274c4f12ad564c1dcd45dc510e34017c
Mecheri Razane <168592564+mecherirazane@users.noreply.github.com>
2026-01-09 20:37:01 +0100

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -6,14 +6,16 @@ terraform {
     }
   }
 
+  # Backend لتخزين حالة Terraform في S3
   backend "s3" {
-    bucket = "arumullaaluruu1"
+    bucket = "razane-eks-bucket1-be8a7cc8"   # ← غيّرنا الاسم ليطابق الـ bucket الجديد
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
 }
+
 provider "aws" {
   region = "us-east-1"
 }
